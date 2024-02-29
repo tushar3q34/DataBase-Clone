41e812f1b5e2878819b6575e0f620cd4b44f3107
iron-cat141 <161105823+iron-cat141@users.noreply.github.com>
2024-02-29 09:51:46 +0530

    Update Task_2.c
    get_page added , do not merge with main i am testing methods for commiting

--- Task_2.c
+++ Task_2.c
@@ -18,6 +18,16 @@ void print_prompt() { printf("db > "); }
 // You can add/delete things from .h file according to your code
 
 // add all fxns from task-1 here
+
+void* get_page(Pager* pager, uint32_t page_num) {
+    void* page = pager->pages[page_num];
+    if (page == NULL) {   // new page
+    // Allocate memory only when we try to access page
+    page = pager->pages[page_num] = malloc(ROWS_PER_PAGE * ROW_SIZE);
+    }
+    return page;
+}
+
 void close_input_buffer(InputBuffer *input_buffer)
 {
     free(input_buffer->buffer);
