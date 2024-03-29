b2a0328f7e663a8425579b553ea24400a9df0ee4
Tushar Jain <tushar3q34@gmail.com>
2024-03-29 23:52:30 +0530

    Cursor.c first changes

--- Cursor.c
+++ Cursor.c
@@ -7,39 +7,76 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include "Task_2.h"
 
+// cursor abstraction
 
-//cursor abstraction
-
-
-    // Create a cursor at the beginning of the table
-    // Create a cursor at the end of the table
-    // Access the row the cursor is pointing to
-    // Advance the cursor to the next row
+// Create a cursor at the beginning of the table
+// Create a cursor at the end of the table
+// Access the row the cursor is pointing to
+// Advance the cursor to the next row
 
 // Use (in further tasks)
 
+// Delete the row pointed to by a cursor
+// Modify the row pointed to by a cursor
+// Search a table for a given ID, and create a cursor pointing to the row with that ID
 
-    // Delete the row pointed to by a cursor
-    // Modify the row pointed to by a cursor
-    // Search a table for a given ID, and create a cursor pointing to the row with that ID
-
-   typedef struct {
-  Table* table;
-  uint32_t row_num;
-  bool end_of_table;  // Indicates a position one past the last element
+typedef struct
+{
+    Table *table;
+    uint32_t row_num;
+    bool end_of_table; // Indicates a position one past the last element
 } Cursor;
 
-Cursor* table_start(){...} 
-Cursor* table_end() {...}
-//creates new cursors
+Cursor *table_start(Table *table)
+{
+    Cursor *cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = false;
+}
+Cursor *table_end(Table *table)
+{
+    Cursor *cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+}
+// creates new cursors
 
 // Now, row_slot() fxn renamed to cursor_location()
 // modify implementation accordingly
-void* cursor_location(Cursor* cursor){...}
+void *cursor_location(Cursor *cursor)
+{
+    uint32_t page_num;
+    uint32_t row_num = cursor->row_num;
+    Table *table = cursor->table;
+    if (row_num % ROWS_PER_PAGE != 0)
+        page_num = row_num / ROWS_PER_PAGE;
+    else
+        page_num = row_num / ROWS_PER_PAGE - 1;
+    void *page = table->pager->pages[page_num];
+    if (page == NULL)
+    {
+        page = table->pager->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
 
 // next row
-void cursor_advance(Cursor* cursor){...}
+void cursor_advance(Cursor *cursor)
+{
+    if (!(cursor->end_of_table))
+    {
+        cursor->row_num++;
+        return cursor;
+    }
+    else
+        printf("End of Table\n");
+}
 
 // use the above cursor fxns (instead of rows) in
 // insert (table_end, etc.)
